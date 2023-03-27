e444103bb46cd07f757f67e31461f97dbb50570f
KrzysztofSZCZ <eruortamo7@gmail.com>
2023-03-27 16:34:49 +0200

    Fix PermMissingElem solution's perfmormance

--- Zadania/PermMissingElem.cpp
+++ Zadania/PermMissingElem.cpp
@@ -3,15 +3,17 @@
 #include <algorithm>
 
 int solution(std::vector<int> &A ){
-
+    if(A.empty())
+        return 1;
     std::sort(A.begin(), A.end());
 
     for(int i = 0; i<A.size(); i++){
 
         if(A[i] != i+1)
             return i+1;
-
     }
+    return (A[A.size() - 1] + 1);
+    
 
 }
 
