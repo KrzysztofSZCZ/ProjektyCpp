6341b9e1af4955faf1f29b699612e2c3e96935d2
KrzysztofSZCZ <eruortamo7@gmail.com>
2023-04-06 07:44:06 +0200

    Add AbsDistinct solution

---
+++ Zadania/AbsDistinct.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+#include<set>
+
+int solution(std::vector<int> &A){
+
+   std::set<int> s;
+
+   for(int i = 0 ; i < A.size(); i++){
+
+    s.insert(std::abs(A[i]));
+   }
+
+    return s.size();
+
+}
+
+
+int main(){
+
+    std::vector<int> vec = {-5,-3-1,0,3,6};
+    std::cout<<solution(vec);
+
+    return 0;
+}
